ce9169e582525a079ba7cf04fc30b5e25da2dce5
Gabriel Henrique <gabhenrique0@gmail.com>
2019-04-06 20:53:27 -0300

    Implement code that makes the app calculate the IMC based on values inserted by the user

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -13,12 +13,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFieldHeight: UITextField!
     @IBOutlet weak var labelResult: UILabel!
     @IBOutlet weak var imageViewResult: UIImageView!
+    @IBOutlet weak var uiViewResult: UIView!
+    
+    var imc: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
     @IBAction func actionCalculateImc(_ sender: Any) {
+        if let weight = Double(textFieldWeight.text!), let height = Double(textFieldHeight.text!) {
+            imc = weight / (height * height)
+            showResults()
+        }
+    }
+    
+    func showResults() {
+        var result = String()
+        var image = String()
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            
+            case 18.5..<25:
+                result = "Peso ideal"
+                image = "ideal"
+            
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobrepeso"
+            
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        labelResult.text = "\(Int(imc)): \(result)"
+        imageViewResult.image = UIImage(named: image)
+        uiViewResult.isHidden = false
+        
+        // This method will lost focus of editing text and hide keyboard
+        view.endEditing(true)
     }
 }
 
